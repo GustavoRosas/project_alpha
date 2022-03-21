de1ae71ef4b936f7c5287e3407dcb147ca1d92d2
Gustavo Rosas <gustavo.rosas97@gmail.com>
2022-03-20 21:53:11 -0300

    ambientation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,6 +34,9 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}" style="padding-right:15px">
+                        {{ __('Register') }}
+                    </a>
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
